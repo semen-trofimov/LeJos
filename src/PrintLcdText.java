14cf2a084a485c6ba3af763c683f2a7b15b8b8ff
semen.trofimov <semiony@gmail.com>
2021-10-28 12:46:07 +0300

    remove all files

--- src/PrintLcdText.java
+++
@@ -1,9 +0,0 @@
-import lejos.hardware.lcd.LCD;
-import lejos.utility.Delay;
-
-public class PrintLcdText {
-    public static void main(String[] args){
-        LCD.drawString("hello!!!",0,0);
-        Delay.msDelay(1000);
-    }
-}
